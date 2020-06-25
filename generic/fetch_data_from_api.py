8ee61d93c5a9db47cf43cbc71c552c0839255f71
mark2357 <marklenton42@gmail.com>
2020-06-25 22:57:20 +1000

    added debug info about caching

--- generic/fetch_data_from_api.py
+++ generic/fetch_data_from_api.py
@@ -7,5 +7,13 @@ requests_cache.install_cache()
 def fetch_data_from_api(url):
     '''returns data from API endpoint'''
     # Make a GET request to fetch the raw HTML content
-    content = requests.get(url).text
+    request = requests.get(url)
+
+    # uncomment if you want do display when data is from cache
+    # if request.from_cache:
+    #     print(f"request from {url} is from cache")
+    # else:
+    #     print(f"request from {url} is not from cache")
+
+    content = request.text
     return content
