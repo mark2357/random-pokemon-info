eb96d8b5205e86c2c6cc175344a48a14be3e5768
mark2357 <marklenton42@gmail.com>
2020-07-02 23:09:38 +1000

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='random_pokemon_info',
-      version='0.1',
+      version='0.1.1',
       description='module that contains functions that return random strings about pokemon',
       author='Mark Lenton',
       license='MIT',
